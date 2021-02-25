54cc36d42a3633bc8edcceabed367437e9909db3
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2021-02-25 13:38:02 +0700

    Refactor layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
 import '../styles/globals.css'
+import Head from 'next/dist/next-server/lib/head.js'
+import styles from '../styles/Home.module.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Shield Finance</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+      <footer className={styles.footer}>
+        &copy; Shield Finance, {new Date().getFullYear()}
+      </footer>
+    </div>
+  )
 }
 
 export default MyApp
