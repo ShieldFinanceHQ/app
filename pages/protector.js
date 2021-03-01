c0e31cda070e3fd965002575dd35305aa035d390
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2021-03-01 12:11:55 +0700

    Implement protector UI demo

--- pages/protector.js
+++ pages/protector.js
@@ -8,7 +8,7 @@ import Col from 'react-bootstrap/Col'
 import { Typeahead } from 'react-bootstrap-typeahead'
 import { markets as marketOptions } from '../src/data.js'
 
-export default function Investor() {
+export default function Protector() {
   // NOTE: markets is an array of 1 element (for consistency with Typeahead component)
   const [markets, set_markets] = useState([])
   const market = markets[0]
@@ -17,19 +17,12 @@ export default function Investor() {
     <Container>
       <Row className="justify-content-md-center">
         <Col lg={6}>
-          {
-            /* TODO:
-             * List<ActiveProtections>
-             * List<AvailableProtections>
-             * Form to create protection
-             */
-          }
-          <h1>Buy protection</h1>
-          <p>
-            Protect your investments from <strong>rug pulls, hacks, exploits</strong>.
-            <br/>
-            Receive payout if the price drops by 50% in 24 hours.
-          </p>
+          <h1>Sell protection</h1>
+          <ul className={styles.list}>
+            <li><strong>Receive premium</strong> from investor who needs protection.</li>
+            <li><strong>Receive premium</strong> in addition to premium while your money is parked in our contract.</li>
+            <li>Lose your money if the premium drops by 50% in 24 hours.</li>
+          </ul>
           <Form>
             <Form.Group controlId="market">
               <Form.Label>Market</Form.Label>
@@ -43,20 +36,27 @@ export default function Investor() {
               />
             </Form.Group>
 
-            <Form.Group controlId="amount">
-              <Form.Label>{market ? `${market.base} amount` : 'Amount'}</Form.Label>
-              <Form.Control type="number" placeholder={`How much ${market ? market.base + ' ' : ''}did you buy?`} />
+            <Form.Group controlId="total">
+              <Form.Label>{market ? `${market.quote} total` : 'Total'}</Form.Label>
+              <Form.Control type="number" placeholder={`How much ${market ? market.quote + ' ' : ''}do you want to provide?`} />
               {/*<Form.Text className="text-muted">*/}
               {/*  {`How much ${market ? market.base + ' ' : ''}do you want to protect?`}*/}
               {/*</Form.Text>*/}
             </Form.Group>
 
-            <Form.Group controlId="price">
-              <Form.Label>{market ? `${market.base} price` : 'Price'}</Form.Label>
-              <Form.Control type="number" placeholder={`What price did you buy at?`} step="any" />
+            <Form.Group controlId="premium">
+              <Form.Label>{market ? `${market.quote} premium` : 'Premium'}</Form.Label>
+              <Form.Control type="number" placeholder={`What premium do you want to receive?`} step="any" />
+              <Form.Text className="text-muted">
+                <ul>
+                  <li>Normal premium = 30%</li>
+                  <li>Higher premium = you will get more money, but your offer may not be taken</li>
+                  <li>Lower premium = your offer will be taken as one of the first, but you will get less money</li>
+                </ul>
+              </Form.Text>
             </Form.Group>
             <Button variant="primary" type="submit">
-              Get protection
+              Sell protection
             </Button>
           </Form>
         </Col>
