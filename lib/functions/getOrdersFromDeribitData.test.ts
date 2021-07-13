3fdefd26b9ed00e20b968b00cdc789f55af49f1d
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2021-07-13 22:37:42 +0700

    Fix getOrdersFromDeribitData

--- lib/functions/getOrdersFromDeribitData.test.ts
+++ lib/functions/getOrdersFromDeribitData.test.ts
@@ -2,13 +2,30 @@ import { test } from '@jest/globals'
 import { expect } from '../util/expect'
 import { promises as fs } from 'fs'
 import { getOrdersFromDeribitData } from './getOrdersFromDeribitData'
+import { Order } from '../interfaces/Order'
 
 test('should get orders from Deribit data', async function () {
   const instrumentsMap = {
-    BTC: JSON.parse(await fs.readFile(`lib/api/deribit/fixtures/get_instruments_BTC.json`, 'utf8')),
-    ETH: JSON.parse(await fs.readFile(`lib/api/deribit/fixtures/get_instruments_ETH.json`, 'utf8')),
+    BTC: JSON.parse(await fs.readFile(`lib/api/deribit/fixtures/BTC_put_instruments.json`, 'utf8')),
+    ETH: JSON.parse(await fs.readFile(`lib/api/deribit/fixtures/ETH_put_instruments.json`, 'utf8')),
   }
-  const orderbooks = JSON.parse(await fs.readFile(`lib/api/deribit/fixtures/get_order_book.json`, 'utf8'))
-  const orders = await getOrdersFromDeribitData(instrumentsMap, orderbooks)
+  const orderbooks = JSON.parse(await fs.readFile(`lib/api/deribit/fixtures/all_put_orderbooks.json`, 'utf8'))
+  const orders = await getOrdersFromDeribitData(instrumentsMap, orderbooks, 33000.50)
   expect(orders.length).to.be.greaterThan(10)
+  expect(orders).to.deep.include({
+    base: 'BTC',
+    quote: 'USDT',
+    protectionAmount: 66.8,
+    protectionPrice: 0.002 * 33000.50,
+    guaranteedPrice: 28000,
+    expirationDate: new Date(1626422400000)
+  } as Order)
+  expect(orders).to.deep.include({
+    base: 'ETH',
+    quote: 'USDT',
+    protectionAmount: 458,
+    protectionPrice: 0.0805 * 33000.50,
+    guaranteedPrice: 1800,
+    expirationDate: new Date(1630051200000)
+  } as Order)
 })
--- lib/functions/getOrdersFromDeribitData.ts
+++ lib/functions/getOrdersFromDeribitData.ts
@@ -3,17 +3,18 @@ import { OrderBook } from '../api/deribit/interfaces/OrderBook'
 import { Order } from '../interfaces/Order'
 import { Instrument } from '../api/deribit/interfaces/Instrument'
 
-export async function getOrdersFromDeribitData(instrumentsMap: { [currency: string]: Instrument }, orderbooks: OrderBook[]) {
+export async function getOrdersFromDeribitData(instrumentsMap: { [currency: string]: Instrument[] }, orderbooks: OrderBook[], BTCUSDPrice: number) {
   return flatten(orderbooks.map((ob) => {
     const [base] = ob.instrument_name.split('-')
-    const instrument = instrumentsMap[base]
+    const instruments = instrumentsMap[base]
+    const instrument = instruments.find((i) => i.instrument_name === ob.instrument_name)
     const quote = 'USDT' // on Deribit it's USD, but we will accept payments in USDT & assume that USDT will maintain its peg
     return ob.asks.map(function ([price, amount]): Order {
       return {
         base,
         quote,
         protectionAmount: amount,
-        protectionPrice: price * ob.index_price,
+        protectionPrice: price * BTCUSDPrice,
         guaranteedPrice: instrument.strike,
         expirationDate: new Date(instrument.expiration_timestamp),
       }
