ef90d95eab604e50081f6f17c12e8ef02eb9c3db
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2021-07-13 21:41:52 +0700

    Fix getOrdersFromDeribitData.test.ts

--- lib/functions/getOrdersFromDeribitData.test.ts
+++ lib/functions/getOrdersFromDeribitData.test.ts
@@ -1,6 +1,14 @@
 import { test } from '@jest/globals'
-import { expect } from 'lib/util/expect'
+import { expect } from '../util/expect'
+import { promises as fs } from 'fs'
+import { getOrdersFromDeribitData } from './getOrdersFromDeribitData'
 
 test('should get orders from Deribit data', async function () {
-
+  const instrumentsMap = {
+    BTC: JSON.parse(await fs.readFile(`lib/api/deribit/fixtures/get_instruments_BTC.json`, 'utf8')),
+    ETH: JSON.parse(await fs.readFile(`lib/api/deribit/fixtures/get_instruments_ETH.json`, 'utf8')),
+  }
+  const orderbooks = JSON.parse(await fs.readFile(`lib/api/deribit/fixtures/get_order_book.json`, 'utf8'))
+  const orders = await getOrdersFromDeribitData(instrumentsMap, orderbooks)
+  expect(orders.length).to.be.greaterThan(10)
 })
